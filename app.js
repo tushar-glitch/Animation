6e303858234de170e2ba2b6238f0017be008108f
Tushar <tusharc20001@gmail.com>
2022-10-03 16:30:37 +0530

    Developed a infinte game loop and adjusted the position of pipes using random positins

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var pipe1 = document.getElementById("pipe1");
 var pipe2 = document.getElementById("pipe2");
-id = setInterval(frame, 5);
+id = setInterval(frame, 1);
 let pos = 0;
   function frame() {
-    if (pos == 500) {
-      clearInterval(id);
+    if (pos == 450) {
+        pos = -20;
+        let pipe1height = Math.floor((Math.random() * 450));
+        pipe1.style.height = pipe1height + "px";
+        pipe2.style.height = (450 - pipe1height) + "px";
     } else {
       pos++; 
         pipe1.style.right = pos + 'px'; 
         pipe2.style.right = pos + 'px'; 
-    }
+      }
+      
   }
