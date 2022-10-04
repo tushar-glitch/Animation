c5f69e15c65a31c30d20f4d26525e35930d343a6
Tushar <tusharc20001@gmail.com>
2022-10-04 22:05:13 +0530

    Fixed various bugs and implemented dark mode in the game

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var pipe1 = document.getElementById("pipe1");
 var pipe2 = document.getElementById("pipe2");
 var bird = document.getElementById("bird");
-var movepipe = setInterval(movePipe, 10);
+var movepipe = setInterval(movePipe, 5);
 var movebird = setInterval(moveBird, 10)
 let pos = 0;
 var pipe1height = parseInt(window.getComputedStyle(pipe1).getPropertyValue("height"));
@@ -9,6 +9,10 @@ var pipe2height = parseInt(window.getComputedStyle(pipe2).getPropertyValue("heig
 var isGameOver = 1;
 var score = 0;
 var highscore = 0;
+var body = document.querySelector('body');
+var scene = document.getElementById('scene');
+var score_list = document.getElementsByClassName('top1');
+
 window.onload = function () {
   highscore = localStorage.getItem("score");
   var a = document.getElementById("hiscore");
@@ -38,30 +42,36 @@ function movePipe() {
   var birdtop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
   var birddown = parseInt(window.getComputedStyle(bird).getPropertyValue("bottom"));
   if (birddown == -15) {
-    console.log("Gameover!!");
+    let Gameover = new Audio("/audio/hit.wav");
+    Gameover.play();
+    console.log("afdaf");
     clearInterval(movepipe);
     clearInterval(movebird);
     if (score > highscore) {
       localStorage.setItem("score", score);
     }
     isGameOver = 0;
+
   }
-  if ((pipe1right+55)>=219&&(pipe1right+60)<=320) {
+  if ((pipe1right + 55) >= 219 && (pipe1right + 60) <= 320) {
     if ((birdtop < (600 - pipe1down - 5)) || (birddown < pipe2top - 20)) {
       console.log("Gameover!!");
       clearInterval(movepipe);
       clearInterval(movebird);
       if (score > highscore) {
-      localStorage.setItem("score", score);
-    }
+        localStorage.setItem("score", score);
+      }
       isGameOver = 0;
+      let Gameover = new Audio("/audio/hit.wav");
+      Gameover.play();
     }
   }
   else if ((pipe1right + 60) == 321) {
     score++;
-    console.log(score);
     var s = document.getElementById('score');
     s.innerText = score;
+    let point = new Audio("/audio/point.wav");
+    point.play();
   }
 }
 
@@ -75,8 +85,41 @@ function moveBird() {
 
 //Spacebar button function
 document.onkeydown = function (e) {
-  if (e.keyCode == 32&&isGameOver) {
+  if (e.keyCode == 32 && isGameOver) {
     var currentbirdpos = parseInt(window.getComputedStyle(bird).getPropertyValue("bottom"));
     bird.style.bottom = (currentbirdpos + 90) + "px";
+    var imgsrc = document.getElementById("bird");
+    imgsrc.style.backgroundImage = "url(/img/up-removebg-preview.png)";
+  }
+}
+document.onkeyup = function (e) {
+  if (e.keyCode == 32) {
+    var imgsrc = document.getElementById("bird");
+    imgsrc.style.backgroundImage = "url(/img/noup-removebg-preview.png)";
+  }
+}
+
+//Toggling of dark and light mode
+function togglemode() {
+  // if (window.getComputedStyle(body).getPropertyValue('background-color')) {
+  //   console.log("asdf");
+  // }
+  if (window.getComputedStyle(body).getPropertyValue('background-color') == 'rgb(255, 255, 255)') {
+    body.style.backgroundColor = 'rgb(37, 37, 37)';
+    scene.style.backgroundColor = 'white';
+    for (let i = 0; i < score_list.length; ++i) {
+      score_list[i].style.color = 'white';
+    }
+    var toggle = document.getElementById('toggle');
+    toggle.style.backgroundImage = "url(/img/moon.jpg)";
+  }
+  else {
+    body.style.backgroundColor = 'white';
+    scene.style.backgroundColor = 'rgb(37, 37, 37)';
+    for (let i = 0; i < score_list.length; ++i) {
+      score_list[i].style.color = 'rgb(37, 37, 37)';
+      var toggle = document.getElementById('toggle');
+      toggle.style.backgroundImage = "url(/img/sun.avif)";
+    }
   }
 }
